bcb357b77d8435a3c36965dfd493ffe14c6bd783
peterqlin <peterql2@illinois.edu>
2026-03-25 14:11:48 -0500

    feat: add mock vehicles and routes data from MTD API examples

---
+++ src/mocks/routes.ts
@@ -0,0 +1,54 @@
+import type { Route } from '../types/mtd'
+
+// Real CUMTD route colors and IDs derived from mtd_api_docs examples
+export const mockRoutes: Route[] = [
+  {
+    route_id: 'BROWN',
+    route_color: 'c5972e',
+    route_text_color: '000000',
+    route_long_name: 'Brown',
+    route_short_name: '10',
+  },
+  {
+    route_id: 'GOLDHOPPER',
+    route_color: 'c5972e',
+    route_text_color: '000000',
+    route_long_name: 'Goldhopper',
+    route_short_name: '11',
+  },
+  {
+    route_id: 'GREEN',
+    route_color: '00a638',
+    route_text_color: '000000',
+    route_long_name: 'Green',
+    route_short_name: '5',
+  },
+  {
+    route_id: 'GREENHOPPER',
+    route_color: '00a638',
+    route_text_color: '000000',
+    route_long_name: 'Greenhopper',
+    route_short_name: '50',
+  },
+  {
+    route_id: 'GREY',
+    route_color: '666666',
+    route_text_color: 'ffffff',
+    route_long_name: 'Grey',
+    route_short_name: '7',
+  },
+  {
+    route_id: 'ILLINI',
+    route_color: 'e84a27',
+    route_text_color: 'ffffff',
+    route_long_name: 'Illini',
+    route_short_name: '22',
+  },
+  {
+    route_id: 'TEAL',
+    route_color: '008080',
+    route_text_color: 'ffffff',
+    route_long_name: 'Teal',
+    route_short_name: '12',
+  },
+]
